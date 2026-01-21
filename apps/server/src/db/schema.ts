96a32b7cefd5da588e630a246ac7ce61a996d0d0
Nabil <lmrb.nabil@gmail.com>
2026-01-21 14:59:56 +0100

    feature: add tables films, posts, comments, ratings

--- apps/server/src/db/schema.ts
+++ apps/server/src/db/schema.ts
@@ -1,9 +1,49 @@
-import { pgTable, integer, varchar } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  integer,
+  varchar,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
 
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  username: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  password: varchar({ length: 255 }).notNull(),
+  createdAt: timestamp().defaultNow(),
+});
 
-export const usersTable = pgTable('users', {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    username: varchar({length: 255}).notNull(),
-    email: varchar({length: 255}).notNull().unique(),
-    password: varchar({length: 255}).notNull()
-})
+export const filmsTable = pgTable("films", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 255 }).notNull(),
+  description: text(),
+});
+
+export const postsTable = pgTable("posts", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar().notNull(),
+  user_id: integer().references(() => usersTable.id),
+  film_id: integer().references(() => filmsTable.id),
+});
+
+export const commentsTable = pgTable("comments", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar().notNull(),
+  user_id: integer()
+    .references(() => usersTable.id)
+    .notNull(),
+  film_id: integer().references(() => filmsTable.id),
+  post_id: integer().references(() => postsTable.id),
+});
+
+export const ratingsTable = pgTable("ratings", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  user_id: integer()
+    .references(() => usersTable.id)
+    .notNull(),
+  movie_id: integer()
+    .references(() => filmsTable.id)
+    .notNull(),
+  rate: integer().notNull(),
+});
