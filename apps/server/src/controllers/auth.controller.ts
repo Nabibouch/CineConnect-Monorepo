c771b9ac426ffa9fce96097aaf9bd5a0854ec84d
gires667 <gires8585@gmail.com>
2026-02-08 11:52:04 +0100

    fix: controller signin to use  service connexion instead of service findUserByEmail

--- apps/server/src/controllers/auth.controller.ts
+++ apps/server/src/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import userService from "../services/user.service.js";
-import argon2 from "argon2";
+//import argon2 from "argon2";
 
 // ✅ SIGNUP = Inscription (createUser)
 export const signUp = async (req: Request, res: Response) => {
@@ -22,13 +22,10 @@ export const signUp = async (req: Request, res: Response) => {
 export const signIn = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
-    const user = await userService.findUserByEmail(email);
-    if (!user) return res.status(400).json({ error: "Utilisateur non trouvé" });
 
-    const validPassword = await argon2.verify(user.password, password);
-    if (!validPassword) return res.status(400).json({ error: "Mot de passe incorrect" });
+    const userWithoutPassword = await userService.connexion({ email, password });
+    if (!userWithoutPassword) return res.status(400).json({ error: "Utilisateur non trouvé" });
 
-    const { password: _, ...userWithoutPassword } = user;
     return res.status(200).json({
       message: "Connexion réussie",
       user: userWithoutPassword,
@@ -38,6 +35,7 @@ export const signIn = async (req: Request, res: Response) => {
     return res.status(500).json({ error: "erreur inconnue" });
   }
 };
+
 // ✅ getAllUsers
 export const getAllUsers = async (req: Request, res: Response) => {
   try {
