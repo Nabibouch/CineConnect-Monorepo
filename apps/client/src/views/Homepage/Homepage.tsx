9e937aa62b114d812e4bd10f8d7ea274337adac5
Nabil <lmrb.nabil@gmail.com>
2026-02-10 11:37:33 +0100

    feature(ui): add a structure for the homepage

---
+++ apps/client/src/views/Homepage/Homepage.tsx
@@ -0,0 +1,12 @@
+import LastFilm from "./components/LastFilm";
+
+
+const Homepage = () => {
+  return (
+    <main className="flex flex-col bg-slate-950 flex-1 gap-6">
+      <LastFilm/>
+    </main>
+  );
+};
+
+export default Homepage;
