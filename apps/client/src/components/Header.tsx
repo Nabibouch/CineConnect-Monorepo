8af9e55b1b413492ef4c1a0a4cc442e25ed6663d
Nabil <lmrb.nabil@gmail.com>
2026-02-10 11:04:27 +0100

    feature(ui):add an header (skeleton)

---
+++ apps/client/src/components/Header.tsx
@@ -0,0 +1,22 @@
+import { Link } from "@tanstack/react-router";
+import { Bell, Search, User } from "lucide-react";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center px-4 text-white bg-slate-800 pt-6">
+      <span className="text-[40px] text-pink-500 font-bold">CinéConnect</span>
+      <nav className="flex gap-4">
+        <Link to="/"> Accueil </Link>
+        <Link to="/film"> film </Link>
+        <span>Favoris</span>
+      </nav>
+      <div className="flex gap-4">
+        <Search color="white" />
+        <Bell color="white" />
+        <User color="white" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
