17f7df2769b43445653cd7efdd148f8d0f027501
Nabil <lmrb.nabil@gmail.com>
2026-02-15 01:28:35 +0100

    feature(ui): label support now icons as props

--- apps/client/src/components/Label.tsx
+++ apps/client/src/components/Label.tsx
@@ -1,12 +1,18 @@
+import type { LucideIcon } from "lucide-react";
+
 type props = {
   title: string;
+  icon?: LucideIcon;
 };
 
-const Label = ({ title }: props) => {
+const Label = ({ title, icon: Icon }: props) => {
   return (
-    <span className="flex items-center w-fit bg-pink-200 rounded-[8px] px-[10px] py-[1px] text-black">
-      {title}
-    </span>
+    <div className="flex gap-2">
+      {Icon && <Icon size={24} />}
+      <span className="flex items-center w-fit bg-pink-200 rounded-[8px] px-[10px] py-[1px] text-black">
+        {title}
+      </span>
+    </div>
   );
 };
 
