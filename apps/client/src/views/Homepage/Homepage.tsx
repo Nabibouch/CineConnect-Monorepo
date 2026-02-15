5766483f2eab3b548f9325b5ac548bcf16b2745f
Nabil <lmrb.nabil@gmail.com>
2026-02-15 01:29:34 +0100

    fix(ui): add minor change on the ui of the homepage

--- apps/client/src/views/Homepage/Homepage.tsx
+++ apps/client/src/views/Homepage/Homepage.tsx
@@ -3,7 +3,7 @@ import LastFilm from "./components/LastFilm";
 
 const Homepage = () => {
   return (
-    <main className="flex flex-col bg-slate-950 flex-1 gap-6">
+    <main className="flex flex-col bg-slate-950 flex-1 gap-6 py-6 px-20">
       <LastFilm/>
     </main>
   );
--- apps/client/src/views/Homepage/components/LastFilm.tsx
+++ apps/client/src/views/Homepage/components/LastFilm.tsx
@@ -1,5 +1,6 @@
 import { useFilmsHomepage } from "../../../hook/useFilms";
 import Label from "../../../components/Label"
+import { Info } from "lucide-react";
 
 const LastFilm = () => {
 
@@ -12,7 +13,8 @@ const LastFilm = () => {
     <div className="flex flex-col gap-6 text-white">
       <Label title="Dernière sortie" />
       <h2 className="text-[40px] font-semibold">{ lastFilm.title }</h2>
-      <p className="w-[1000px]">{ lastFilm.description }</p>
+      <p className="w-[1000px]">{lastFilm.description}</p>
+      <Label title="Plus d'info" icon={Info} />
     </div>
   )
 }
