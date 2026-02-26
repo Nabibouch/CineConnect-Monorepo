4866011fef1a2c3a09eed90904e14623ecaaacf8
Nabil <lmrb.nabil@gmail.com>
2026-02-26 18:28:56 +0100

    feature(back/controllers): add getAllRatings controller

---
+++ apps/server/src/controllers/ratings.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import ratingService from "../services/ratings.service.js";
+
+export const addRating = async (req : Request, res: Response) => {
+    try {
+        const newRating = ratingService.createRating(req.body);
+        return res.status(201).json({message: "note ajouté", note: newRating})
+    } catch (error) {
+        if (error instanceof Error) {
+            return res.status(400).json({error: error.message})
+        }
+    }
+};
+
+export const getAllRatings = async (req: Request, res: Response) => {
+    try {
+        const allRatings = ratingService.findAllRatings();
+        return res.status(200).json({allRatings});
+    } catch (error) {
+        if (error instanceof Error) {
+            return res.status(400).json({error: error.message});
+        }
+    }
+}
+
