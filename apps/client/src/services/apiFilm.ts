162d3a8292f9cfba08142e0c3bb784dec001435e
Nabil <lmrb.nabil@gmail.com>
2026-03-01 11:48:02 +0100

    update(front/services): add an interface for Post, Comment and edit Film for more precisions

--- apps/client/src/services/apiFilm.ts
+++ apps/client/src/services/apiFilm.ts
@@ -5,12 +5,37 @@ export interface Rating {
   rate: number;
 }
 
+export interface Comment {
+  id: number;
+  content: string;
+  user_id: number;
+  film_id: number | null;
+  post_id: number | null;
+}
+
+export interface Post {
+  id: number;
+  title: string;
+  user_id: number;
+  film_id: number;
+  comments: Comment[];
+}
+
 export interface Film {
-  id: string;
+  id: number;
   title: string;
   description: string;
-  released_date: string;
-
+  poster_url: string | null;
+  author: string | null;
+  language: string | null;
+  trailer: string | null;
+  actors: string | null;
+  awards: string | null;
+  released_date: string | null;
+  created_at: string;
+  posts: Post[];
+  comments: Comment[];
+  ratings: Rating[];
 }
 const url = import.meta.env.VITE_API_URL;
 
