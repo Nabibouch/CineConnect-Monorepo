1c1138dd65b7a39c5ef22500240091a8bcc79f6c
Nabil <lmrb.nabil@gmail.com>
2026-03-14 16:02:12 +0100

    refactor(front/services): import type film from utils

--- apps/client/src/services/apiFilm.ts
+++ apps/client/src/services/apiFilm.ts
@@ -1,42 +1,4 @@
-export interface Rating {
-  id: number;
-  user_id: number;
-  film_id: number;
-  rate: number;
-}
-
-export interface Comment {
-  id: number;
-  content: string;
-  user_id: number;
-  film_id: number | null;
-  post_id: number | null;
-}
-
-export interface Post {
-  id: number;
-  title: string;
-  user_id: number;
-  film_id: number;
-  comments: Comment[];
-}
-
-export interface Film {
-  id: number;
-  title: string;
-  description: string;
-  poster_url: string | null;
-  author: string | null;
-  language: string | null;
-  trailer: string | null;
-  actors: string | null;
-  awards: string | null;
-  released_date: string | null;
-  created_at: string;
-  posts: Post[];
-  comments: Comment[];
-  ratings: Rating[];
-}
+import type { Film } from "../utils/types";
 const url = import.meta.env.VITE_API_URL;
 
 export const apiFilm = {
