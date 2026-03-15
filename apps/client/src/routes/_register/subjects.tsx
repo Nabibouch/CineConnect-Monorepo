805757b9250d5e518646b93a5f7bf539d8910560
Nabil <lmrb.nabil@gmail.com>
2026-03-15 14:07:09 +0100

    feature(front/routes): add a route for subjects page

---
+++ apps/client/src/routes/_register/subjects.tsx
@@ -0,0 +1,8 @@
+import { createFileRoute } from '@tanstack/react-router'
+import Subjects from '../../pages/Subjects/Subjects'
+
+export const Route = createFileRoute('/_register/subjects')({
+    component: Subjects,
+})
+
+
