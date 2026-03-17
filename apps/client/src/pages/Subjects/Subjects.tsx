0240f980c181523dbaefa7cbeb266f4cc2d4d677
Nabil <lmrb.nabil@gmail.com>
2026-03-17 13:07:10 +0100

    feature(front/pages): add a link to for each subjectCard

--- apps/client/src/pages/Subjects/Subjects.tsx
+++ apps/client/src/pages/Subjects/Subjects.tsx
@@ -1,5 +1,6 @@
 import { useSubjects } from '../../hook/useSubjects';
 import SubjectCard from './components/SubjectCard';
+import { Link } from '@tanstack/react-router';
 
 const Subjects = () => {
     const { data: subjects, isLoading, error } = useSubjects();
@@ -12,7 +13,9 @@ const Subjects = () => {
             <h1 className="text-3xl font-semibold tracking-wide">Subjects</h1>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                 {subjects?.map((subject) => (
-                    <SubjectCard key={subject.id} subject={subject} />
+                    <Link to="/subjects/$id" params={{ id: subject.id.toString() }} key={subject.id}>
+                        <SubjectCard subject={subject} />
+                    </Link>
                 ))}
             </div>
         </div>
