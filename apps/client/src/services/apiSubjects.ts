90cd18854dc72b613c8136dccde619d8bec07edc
Nabil <lmrb.nabil@gmail.com>
2026-03-22 19:09:12 +0100

    feature(front/services): add createSubject which use method post request

--- apps/client/src/services/apiSubjects.ts
+++ apps/client/src/services/apiSubjects.ts
@@ -1,6 +1,8 @@
 import type { Post } from "../utils/types";
 const url = import.meta.env.VITE_API_URL;
 
+export type CreateSubjectPayload = Partial<Omit<Post, 'id' | 'comments'>>;
+
 export const apiSubjects = {
   getAllSubjects: async (): Promise<Post[]> => {
     const response = await fetch(`${url}/posts`);
@@ -11,6 +13,22 @@ export const apiSubjects = {
   getSubjectById: async (id: string): Promise<Post> => {
     const response = await fetch(`${url}/posts/${id}`);
     if (!response.ok) throw new Error('Erreur lors de la récupération du commentaire');
+    return response.json();
+  },
+  
+  createSubject: async (payload: CreateSubjectPayload): Promise<Post> => {
+    const response = await fetch(`${url}/posts`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+    });
+
+    if (!response.ok) {
+        throw new Error('Erreur lors de la création du sujet');
+    }
+
     return response.json();
   }
 };
