c5d060c1e0114cc327d2ede56d01d5bd8da3d802
gires667 <gires8585@gmail.com>
2026-03-26 12:04:30 +0100

    feat: creation of the footer components

--- apps/client/src/components/Footer.tsx
+++ apps/client/src/components/Footer.tsx
@@ -1,29 +1,23 @@
-import { RiGithubFill, RiFacebookCircleFill ,RiLinkedinBoxFill, RiTwitterXFill   } from '@remixicon/react';
-
+import Icon from "./footerelement/footericon";
+import Footerlinks from "./footerelement/footerlinks";
+import Subst from "./footerelement/subtitle";
 
 const Footer = () => {
     return(
-        <footer className='flex items-center justify-start bg-light_slayer '>
-            <div className='flex flex-col m-4 '>
-                <span className='font-bold text-[40px] text-white'>Movie<span className='text-red-500'>Tune</span></span>
-                <span className='text-white'>Lorem ipsum dolor sit amet, 
-                    consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt 
-                    ut labore et dolore magna aliqua</span>
-
-                    <div>
-                    <p className='text-red-500'>follow us on:</p>
-                    <div className='flex flex-row justify-between px-12'>
-                        <RiGithubFill />
-                        <RiFacebookCircleFill />
-                        <RiLinkedinBoxFill />
-                        <RiTwitterXFill />
-
-                    </div>
-
-                    </div>
-
+        <footer className='flex items-start bg-slayer h-72 pt-4'>
+                        
+            <div className="mx-28 flex flex-col">
+                <Subst  />
+                    <Icon />
             </div>
+
+            <div className="flex flex-row justify-between gap-40">            
+                <Footerlinks title='QUICK' title2="LINKS" links={["Home","Actors","Compressions","movies","services"]}/>
+                <Footerlinks title='' title2="support" links={["faqs","terms of use","privacy and policy"]}/>
+                <Footerlinks title='' title2="article" links={["Blogs","careers",]}/>
+            </div>          
+                     
+                        
 
         </footer>
     )
