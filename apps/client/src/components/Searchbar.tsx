a6230002077e21f6f503f279e478e7cef2c358dc
Nabil <lmrb.nabil@gmail.com>
2026-03-30 20:12:18 +0200

    feature(front/components): add searchBar components

--- apps/client/src/components/Searchbar.tsx
+++ apps/client/src/components/Searchbar.tsx
@@ -1,5 +1,12 @@
 import { RiSearchLine } from "@remixicon/react";
-const SearchBar = () => {
+import type { ChangeEvent } from "react";
+
+type SearchBarProps = {
+  value: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
+
+const SearchBar = ({ value, onChange }: SearchBarProps) => {
 
     return (
         <div className="flex items-center w-[564px] h-[48px] bg-[#FFDEE6] rounded-2xl px-4">
@@ -9,6 +16,8 @@ const SearchBar = () => {
         <input
             placeholder="Rechercher un film..."
             type="text"
+            value={value}
+            onChange={onChange}
             className="bg-transparent outline-none flex-1 ml-3 text-black"
                 />
   
