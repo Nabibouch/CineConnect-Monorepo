82c3069d17be5a9c072f3eb8b1ed249747dd6fed
Nabil <lmrb.nabil@gmail.com>
2026-03-30 11:13:21 +0200

    update(back/routes): add 2 endpoint delete and add category

--- apps/server/src/routes/films.route.ts
+++ apps/server/src/routes/films.route.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import {
   addFilm,
+  addCategoryToFilm,
+  deleteCategoryFromFilm,
   deleteOneFilm,
   getAllFilms,
   getOneFilm,
@@ -12,6 +14,8 @@ const router = express.Router();
 
 router.post("/", addFilm);
 router.get("/", getAllFilms);
+router.post("/:id/categories", addCategoryToFilm);
+router.delete("/:id/categories/:category", deleteCategoryFromFilm);
 router.put("/:id", updateOneFilm);
 router.delete("/:id", deleteOneFilm);
 router.get("/:id", getOneFilm);
