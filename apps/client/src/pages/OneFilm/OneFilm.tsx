43e1753dd906b70399f39c64b3c29ebb98728f42
Nabil <lmrb.nabil@gmail.com>
2026-03-30 21:27:51 +0200

    feature(front/pages): list coms

--- apps/client/src/pages/OneFilm/OneFilm.tsx
+++ apps/client/src/pages/OneFilm/OneFilm.tsx
@@ -18,7 +18,25 @@ const OneFilm = () => {
         <div className="relative min-h-screen space-x-2 bg-toxic text-white">
             <FilmHeader />
             <div className='px-32'>
-                <FilmDetails film={film}/>
+                <FilmDetails film={film} />
+                <div>
+                    <h2 className="text-xl text-white font-semibold mb-2">Commentaires</h2>
+
+                    {film.comments?.length ? (
+                        <ul className="space-y-3">
+                            {film.comments.map((comment) => (
+                                <li
+                                    key={comment.id}
+                                    className="p-4 border border-slate-800 bg-slate-900/60 rounded-2xl shadow-sm"
+                                >
+                                    <p className="text-white font-medium">{comment.title}</p>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p className="text-white">Aucun commentaire</p>
+                    )}
+                </div>
                 <NewCom film={film} />
                 <NewSubject />
             </div>
